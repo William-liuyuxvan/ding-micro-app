6e8594893a24d6de8c82243a9cf44af218a4d872
william-liuyuxvan <william1215liu@163.com>
2024-12-28 17:50:36 +0800

    chore: 初始化项目和文档

---
+++ front/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <header>
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </div>
+    </header>
+    <h1>HelloWorld</h1>
+    <RouterView />
+  </div>
+</template>
+
+<style scoped></style>
