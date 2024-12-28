7bec3aabd1f40fa0deb3b644f574599cbb1416ab
william-liuyuxvan <william1215liu@163.com>
2024-12-28 18:12:50 +0800

    chore: 初始化后的vant熟悉

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,7 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from '@vant/auto-import-resolver';
+
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
@@ -9,6 +13,12 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    AutoImport({
+      resolvers: [VantResolver()],
+    }),
+    Components({
+      resolvers: [VantResolver()],
+    }),
   ],
   resolve: {
     alias: {
