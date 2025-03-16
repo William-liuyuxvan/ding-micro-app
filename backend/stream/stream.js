677ee210bb4986d38c515ed995c088c652361ca0
william-liuyuxvan <william1215liu@163.com>
2025-03-16 11:41:45 +0800

    chores: 使用https进行机器人回复对话

--- backend/stream/stream.js
+++ backend/stream/stream.js
@@ -1,5 +1,6 @@
-import { DWClient } from "dingtalk-stream-sdk-nodejs"
+import { DWClient, EventAck } from "dingtalk-stream-sdk-nodejs"
 import config from "../datas/ding.config.json" with {type: "json"}
+import https from "https"
 
 export const initStream = () => {
     console.log('stream 接入成功');
@@ -47,9 +48,76 @@ export const initStream = () => {
 
     client.registerCallbackListener('/v1.0/im/bot/messages/get', async (res) => {
         // 注册机器人回调事件
+        /* 当你@机器人发送“你好”，服务端 res 接收消息格式如下：
+        {
+            specVersion: '1.0',
+            type: 'CALLBACK',
+            headers: {
+                appId: 'b0a83318-acfd-4d14-b2fc-c6ad2f23637e',
+                connectionId: '601fca83-0213-11f0-a3bc-2283bad79338',
+                contentType: 'application/json',
+                messageId: '21660eee_11c0_1956e7a6274_174c910',
+                time: '1742094274945',
+                topic: '/v1.0/im/bot/messages/get'
+            },
+            data: {
+                "senderPlatform":"Win",
+                "conversationId":"cidDtn+wtv1GGxBrhT6WVZF9A==",
+                "atUsers":[{"dingtalkId":"$:LWCP_v1:$S0VnqLeXKqQEJ9T4jvKYXFzwF4L6g1FN"}],
+                "chatbotCorpId":"{chatbotCorpId}",
+                "chatbotUserId":"{chatbotUserId}",
+                "msgId":"msg1U0rEvGa0pElynCn2mNxUQ==",
+                "senderNick":"{发送人员}",
+                "isAdmin":true,
+                "senderStaffId":"{senderStaffId}",
+                "sessionWebhookExpiredTime":1742099674872,
+                "createAt":1742094274554,
+                "senderCorpId":"{senderCorpId}",
+                "conversationType":"2",
+                "senderId":"{senderId}",
+                "conversationTitle":"天秉义test",
+                "isInAtList":true,
+                "sessionWebhook":"{sessionWebhook}",
+                "text":{
+                    "content":" asdf "
+                },
+                "robotCode":"{robotCode}",
+                "msgtype":"text"
+        }
+         */
         console.log("收到消息", res);
         const { messageId } = res.headers;
         const { text, senderStaffId, sessionWebhook } = JSON.parse(res.data);
+
+        const data = JSON.stringify({
+            'msgtype': 'text',
+            'text': {
+                'content': '我是一段回复文字' + `${senderStaffId}`,
+            },
+            'at': {
+                'atUserIds': [senderStaffId]
+            }
+        })
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }
+        const req = https.request(sessionWebhook, options, (res) => {
+            console.log(`状态码：${res.statusCode}`);
+            res.on('data', (d) => {
+                console.log('data: ' + d);
+            })
+        });
+        req.on('error', (error) => {
+            console.error("error: ", error);
+        });
+        req.write(data);
+        req.end();
+
+        client.send(res.headers.messageId, res.data);
+        return { status: EventAck.SUCCESS, message: "ok" }; // message 属性可以是任意字符串
     })
         .connect();
 
